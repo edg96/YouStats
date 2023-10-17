a152dbd4847da360ed8efc7cc4e8dd18bab0bcea
Deer Me <x.deerme@gmail.com>
2023-10-18 02:57:22 +0300

    Initial commit

---
+++ src/youstats/main.py
@@ -0,0 +1,15 @@
+__author__ = 'Dragos-Gabriel Enache'
+__copyright__ = 'N/A'
+__credits__ = ['N/A']
+
+__license__ = 'N/A'
+__version__ = "1.0.1"
+__maintainer__ = 'Dragos-Gabriel Enache'
+__email__ = 'edragosgabriel@gmail.com'
+__status__ = 'Development'
+
+__all__ = []
+
+
+if __name__ == '__main__':
+    pass
